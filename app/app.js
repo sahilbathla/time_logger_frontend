2318ef2626151f43ce998f792335b26fc77e9b60
Sahil Batla <sahil.batla@wingify.com>
2017-07-24 09:13:01 +0530

    Use app as timeLoggerApp

--- app/app.js
+++ app/app.js
@@ -1,6 +1,5 @@
 define('app', ['angular', 'angular-route', 'geolocation'], function (angular, ngRoute, ngGeolocation) {
 	'use strict';
-	var app = angular.module('openWeatherApp', ['ngRoute', 'ngGeolocation']);
-	app.constant('OPEN_WEATHER_APP_ID', '44db6a862fba0b067b1930da0d769e98');
+	var app = angular.module('timeLoggerApp', ['ngRoute', 'ngGeolocation']);
 	return app;
 });
